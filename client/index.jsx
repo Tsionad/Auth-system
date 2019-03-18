be0c48fb757c06cf96e44587892b016a1b29d652
Tsion Adefres <tadefres@gmail.com>
2019-03-17 21:57:52 -0700

    react setup w/ hot-loader

---
+++ client/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>React/GraphQL Authentication system</div>,
+  document.getElementById('app'),
+);
+
+module.hot.accept();
